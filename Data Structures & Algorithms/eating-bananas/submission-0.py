b2c59132a563031a3e32a504aa2c6f1b311f974b
Yekatierina Korotova <31920907+KateKorotova@users.noreply.github.com>
2026-07-31 01:02:21 -0700

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.py
@@ -0,0 +1,19 @@
+import math 
+
+class Solution:
+    def get_h(self, piles, k):
+        return sum([math.ceil(pile/k) for pile in piles])
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left = 1
+        right = max(piles)
+        while left < right:
+            mid = (left + right)//2
+            curr_h = self.get_h(piles, mid)
+            if curr_h <= h:
+                right = mid
+            elif curr_h > h:
+                left = mid + 1
+        return right
+        
+
