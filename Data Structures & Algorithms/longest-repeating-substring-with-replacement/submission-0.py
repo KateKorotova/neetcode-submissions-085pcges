e9489d2cd1604edca6639380f733a12932681521
Yekatierina Korotova <31920907+KateKorotova@users.noreply.github.com>
2026-08-13 16:18:21 +0700

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        freq_map = defaultdict(int)
+        max_w = 0
+        l, r = 0, 0
+        while r < len(s):
+            freq_map[s[r]] += 1
+            maxf = max(freq_map.values())
+            L = (r - l) + 1
+            # max_w = max(max_w, L)
+            if (L - maxf) > k:
+                max_w = max(max_w, L - 1)
+                freq_map[s[l]] -= 1
+                l += 1
+            r += 1
+        return max(max_w, r-l)
+
+
+
+        
